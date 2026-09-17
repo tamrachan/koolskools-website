5e2ffc8439a4c8737b8061bf49dd1962df209f8b
MutantCacti <mutantcacti@gmail.com>
2026-09-17 15:05:27 +0100

    Add password gate with hardcoded hash

--- KoolSkools-website/src/App.tsx
+++ KoolSkools-website/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import PasswordGate from './components/PasswordGate';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
 import About from './pages/About';
@@ -9,16 +10,18 @@ import Basket from './pages/Basket';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/basket" element={<Basket />} />
-      </Routes>
-    </BrowserRouter>
+    <PasswordGate>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/basket" element={<Basket />} />
+        </Routes>
+      </BrowserRouter>
+    </PasswordGate>
   );
 }
